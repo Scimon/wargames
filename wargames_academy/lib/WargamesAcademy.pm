7f97ffc7e35863b9651678ca96001ee6b7e8ba02
Simon Proctor <simon.proctor@gmail.com>
2012-03-04 15:36:00 +0000

    Signed-off-by: Simon Proctor <simon.proctor@gmail.com>

--- wargames_academy/lib/WargamesAcademy.pm
+++ wargames_academy/lib/WargamesAcademy.pm
@@ -8,8 +8,8 @@ sub startup {
   my $self = shift;
   $self->secret( "ddkl23dijfop2v doijecwoimd" );
 
-  my $sysusers = SystemUsers->new;
-  $self->helper( system_users => sub { return $users });
+  my $sysusers = SystemUser->new;
+  $self->helper( system_users => sub { return $sysusers });
 
   # Routes
   my $r = $self->routes;
