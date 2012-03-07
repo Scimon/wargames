3235d71a42b17e030f3403f75e02f9d7b6ae38cb
Simon Proctor <simon.proctor@gmail.com>
2012-03-07 21:14:01 +0000

    Add_hex
    Signed-off-by: Simon Proctor <simon.proctor@gmail.com>

--- wargames_academy/lib/Game/HexMap.pm
+++ wargames_academy/lib/Game/HexMap.pm
@@ -7,6 +7,11 @@
 	has 'hexes', is => 'rw', isa => 'ArrayRef[Game::Hex]', default => sub { [] }, 'trigger' => \&_set_id_for_hexes;
 	has 'id', is => 'rw', 'isa' => 'Int', 'default' => sub { -1 }, 'trigger' => \&_set_id_for_hexes;
 	
+	sub add_hex {
+		my $self, $hex = @_;
+		$self->hexes( [@{$self->hexes()},$hex] );
+	}
+	
 	sub _set_id_for_hexes {
 		my $self = shift;
 		if ( $self->id() > -1 ) {
