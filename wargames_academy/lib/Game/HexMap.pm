b8b09d16c971c1923ff4d01a83d1c95cf761b5eb
Simon Proctor <simon.proctor@gmail.com>
2012-03-07 20:50:23 +0000

    Added default. Signed-off-by: Simon Proctor <simon.proctor@gmail.com>

--- wargames_academy/lib/Game/HexMap.pm
+++ wargames_academy/lib/Game/HexMap.pm
@@ -4,7 +4,7 @@
 	use Moose;
 	use Game::Hex;
 	
-	has 'hexes', is => 'rw', isa => 'ArrayRef[Game::Hex]';
+	has 'hexes', is => 'rw', isa => 'ArrayRef[Game::Hex]', default => sub { [] };
 	
 	'module returns true';
 }
