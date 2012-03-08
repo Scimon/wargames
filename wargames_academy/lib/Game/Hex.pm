662e48b8448212e3610fd1d6fcb068bc9cb8d423
Simon Proctor <simon.proctor@gmail.com>
2012-03-08 11:19:45 +0000

    Adding slopes

--- wargames_academy/lib/Game/Hex.pm
+++ wargames_academy/lib/Game/Hex.pm
@@ -3,11 +3,17 @@
 	
 	use Moose;
 	use Game::HexType;
+	use Game::Hex::Vector;
 	
 	has 'x', is => 'rw', isa => 'Int';
 	has 'y', is => 'rw', isa => 'Int';
 	has 'map', is => 'rw', isa => 'Game::HexMap';
 	has 'hextype', is => 'rw', isa => 'Game::HexType';
+	has 'height_base', is => 'rw', isa => 'Int';
+	has 'height_end', is => 'rw', isa => 'Int';
+	has 'slope', is => 'rw', isa => 'Game::Hex::Vector';
 	
 	'module returns true';
 }
+
+
