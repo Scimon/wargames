a11280a32263c7f225996458cd8c3f6948187f02
Simon Proctor <simon.proctor@gmail.com>
2012-03-15 17:30:27 +0000

    updates

--- db/base-tables-dev.sql
+++ db/base-tables-dev.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS wargames_dev.hexmap;
 CREATE TABLE wargames_dev.hexmap (
 	`id` INT(10) unsigned NOT NULL auto_increment,
-	`name` VARCHAR(255),
+	`name` VARCHAR(255) NOT NULL DEFAULT '',
 	PRIMARY KEY (`id`)
 );
 
@@ -11,8 +11,8 @@ DROP TABLE IF EXISTS wargames_dev.hextype;
 DROP TABLE IF EXISTS wargames_dev.hex;
 CREATE TABLE wargames_dev.hex (
 	`map_id` INT(10) unsigned NOT NULL,
-	`x` INT(10) NOT NULL,
-	`y` INT(10) NOT NULL,
-	`hextype` VARCHAR(255) unsigned NOT NULL,
+	`x` INT(10) NOT NULL DEFAULT 0,
+	`y` INT(10) NOT NULL DEFAULT 0,
+	`hextype` VARCHAR(255) NOT NULL DEFAULT '',
 	PRIMARY KEY ( `map_id`, `x`, `y` )
 );
