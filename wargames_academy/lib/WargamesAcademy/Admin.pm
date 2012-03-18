975ee54bcb68a787f2835eb19cbf0af5f837cd8b
Simon Proctor <scimon@khanate.co.uk>
2012-03-18 14:40:37 +0000

    Updates

--- wargames_academy/lib/WargamesAcademy/Admin.pm
+++ wargames_academy/lib/WargamesAcademy/Admin.pm
@@ -6,6 +6,7 @@ use warnings;
 use Mojo::Base 'Mojolicious::Controller';
     
 use Game::Collection::HexMap;
+use Game::HexMap;
 
 sub index {
     my $self = shift;
@@ -15,12 +16,25 @@ sub index {
     $self->render;
 }
 
+sub map {
+    my $self = shift;
+    return $self->redirect_to('/system_login') unless $self->session('user');
+
+    my $id = $self->stash( 'id' );
+    my $map = new Game::HexMap( 'id' => $id );
+    $map->load();
+    
+    $self->stash( 'title' => 'Edit : ' . $map->name(), 
+		  'Map' => $map );
+
+    $self->render;
+}
+
 sub maps {
     my $self = shift;
     return $self->redirect_to('/system_login') unless $self->session('user');
 
-    $self->stash( 'title' => "Maps List", 'collection' => new Game::Collection::HexMap() );
-  
+    $self->stash( 'title' => "Maps List", 'collection' => new Game::Collection::HexMap() ); 
 
     $self->render;
 }
