966585d36cfc6aff17ceccd062b8f69d1a230d5b
Simon Proctor <simon.proctor@gmail.com>
2012-03-26 11:13:12 +0100

    GreyScale Hex before highlighting

--- wargames_academy/public/js/Game/Hex.js
+++ wargames_academy/public/js/Game/Hex.js
@@ -84,8 +84,9 @@ var Game_Hex_View = Backbone.View.extend( {
 	    this.model.get('hextype').draw( ctx, this.model );
 
 	    if ( this.model.get('selected') ) {
-		ctx.fillStyle = 'rgba( 255,0,0,0.25 )';
-		ctx.fillRect(0,0,width,height);
+			this.greyscale( ctx, width, height );
+			ctx.fillStyle = 'rgba( 128,0,0,0.25 )';
+			ctx.fillRect(0,0,width,height);
 	    }
 
 	    ctx.restore();
@@ -93,6 +94,21 @@ var Game_Hex_View = Backbone.View.extend( {
 	    ctx.stroke();
 
 	    return this;
+	},
+	'greyscale' : function(ctx,width,height) {
+		var imgPixels = ctx.getImageData(0, 0, width, height);
+
+		for( var y = 0; y < imgPixels.height; y++){
+			for( var x = 0; x < imgPixels.width; x++){
+				var i = (y * 4) * imgPixels.width + x * 4;
+				var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+				imgPixels.data[i] = avg;
+				imgPixels.data[i + 1] = avg;
+				imgPixels.data[i + 2] = avg;
+			}
+		}
+		ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+
 	},
 	'follow_path' : function( ctx, list ) {
 	    ctx.beginPath();	
