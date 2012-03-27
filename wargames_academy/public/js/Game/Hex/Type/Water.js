8da3f892d47dd9137e7a0b3bfbb1a129eaa1968e
Simon Proctor <simon.proctor@gmail.com>
2012-03-27 09:26:40 +0100

    Basic code for Arid, Rocky and Water type

--- wargames_academy/public/js/Game/Hex/Type/Water.js
+++ wargames_academy/public/js/Game/Hex/Type/Water.js
@@ -1,4 +1,21 @@
 var Game_Hex_Type_Water = Game_Hex_Type.extend( {
 	'draw' : function( ctx, hex ) {
+	    var height = hex._half_height * 2;
+	    var width = hex._half_width * 2;
+	    var land_height = hex.get('height');
+	    var random = hex.random();
+		var colour = ( 80 + ( land_height  * 40 ) );
+	    ctx.fillStyle = 'rgb( 0,0, ' + colour + ' )';
+	    var base = 110 + ( land_height * 40 );
+	    ctx.fillRect(0,0,width,height);
+	    for ( var g = base; g > base - 50; g = g - 10 ) {
+		ctx.fillStyle = 'rgba( 0, 0, ' + g + ', 0.08 )';
+		ctx.beginPath();
+		var rad = Math.floor( ( random() * ( height / 2 ) ) + ( height / 4 ) );
+		var cx = Math.floor( random() * ( width - ( rad * 2 ) ) ) + rad; 
+		var cy = Math.floor( random() * ( height - ( rad * 2 ) ) ) + rad; 
+		ctx.arc( cx, cy, rad, 0 , Math.PI * 2, false );
+		ctx.fill();
+
 	}
 }
