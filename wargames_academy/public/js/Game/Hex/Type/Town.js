386eb12e3adc1a22726762c6fb03af53e12fbb1b
Simon Proctor <simon.proctor@gmail.com>
2012-04-05 10:25:55 +0100

    Small fix for towns

--- wargames_academy/public/js/Game/Hex/Type/Town.js
+++ wargames_academy/public/js/Game/Hex/Type/Town.js
@@ -7,17 +7,21 @@ var Game_Hex_Type_Town = Game_Hex_Type.extend( {
 	    var base = ( land_height  * 40 );
 	    ctx.fillStyle = 'rgb( ' + base + ', ' + base + ', ' + base + ' )';
 	    ctx.fillRect(0,0,width,height);
+		var rects = [];
 	    for ( var g = 0; g < 10; g++ ) {
-		ctx.fillStyle = 'rgba( 100, 100, 100, 1)';
-		ctx.strokeStyle = 'rgba( 0, 0, 0, 1 )';
-		ctx.lineWidth = 2;
-		var bh = Math.floor( 1 + ( random() * 10 ) * ( height / 20 ) ); 
-		var bw = Math.floor( 1 + ( random() * 10 ) * ( width / 20 ) ); 
-		var x = Math.floor( random() * 10 * ( ( ( width - bw ) / 10 ) ) );
-		var y = Math.floor( random() * 10 * ( ( ( height - bh ) / 10 ) ) );
-		ctx.strokeRect(x,y,bw,bh);
-		ctx.fillRect(x,y,bw,bh);
+			ctx.fillStyle = 'rgba( 100, 100, 100, 1)';
+			ctx.strokeStyle = 'rgba( 0, 0, 0, 1 )';
+			ctx.lineWidth = 2;
+			var bh = Math.floor( 1 + ( random() * 10 ) * ( height / 20 ) ); 
+			var bw = Math.floor( 1 + ( random() * 10 ) * ( width / 20 ) ); 
+			var x = Math.floor( random() * 10 * ( ( ( width - bw ) / 10 ) ) );
+			var y = Math.floor( random() * 10 * ( ( ( height - bh ) / 10 ) ) );
+			ctx.strokeRect(x,y,bw,bh);
+			rects.push( [x,y,bw,bh] );
+		
 	    }
-
+		for ( var o,i=0; o = rects[i]; o++ ) {
+			ctx.fillRect(o[0],o[1],o[2],o[3]);
+		}
 	}
 });
