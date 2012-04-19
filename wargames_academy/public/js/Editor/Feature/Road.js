60df6bae0f962077b80b24606800fb62e1d9f5e4
Simon Proctor <simon.proctor@gmail.com>
2012-04-19 10:22:48 +0100

    Should really stop doing this in the office

--- wargames_academy/public/js/Editor/Feature/Road.js
+++ wargames_academy/public/js/Editor/Feature/Road.js
@@ -1,7 +1,26 @@
 var Editor_Feature_Road = Editor_Feature.extend({
 	'render' : function() {
-	    this.$el.html( "Road" );
+	    var start = this.position_select( 'start', this.model.get('start') );
+	    var end = this.position_select(  'end', this.model.get('end') );
+		
+	    this.$el.html( "<p>Road : Start " + start + " End " + end + "</p>" );
+	    this.start_select = $(this.model.cid + ':start');
+	    this.end_select = $(this.model.cid + ':end');
+	    this.delegateEvents();
 	    return this;
+	},
+	'events' : {
+	    'change select.start' : 'update_points',
+	    'change select.end' : 'update_points',
+	},
+	'update_points' : function( e ) {
+	    var selector = e.target;
+	    var criteria = {};
+	    criteria[selector.className] = selector.options[selector.selectedIndex].value;
+	    this.model.set( criteria, { wait : true } );
+	},
+	'create_new' : function() {
+	    return new Game_Hex_Feature_Road( { 'type' : 'Road', 'start' : 'c0', 'end' : 'c3' } );
 	}
 
     });
--- wargames_academy/public/js/Game/Hex/Feature/Road.js
+++ wargames_academy/public/js/Game/Hex/Feature/Road.js
@@ -1,3 +1,23 @@
 var Game_Hex_Feature_Slope = Game_Hex_Feature.extend({
+	'draw' : function( ctx, hex, grid ) {
+	    var canvas = this.blankCanvas( hex );
+	    var newCtx = canvas.getContext( '2d' );
+	    
+	    var start = hex.processPoint( this.get('start') );
+	    var end = hex.processPoint( this.get('end') );
+	    var points = this.curvePoints( start, end, hex.random(), hex.get('scale') );
+		
+	    newCtx.strokeStyle = 'rgba( 250, 230, 120, 1 )';
+	    newCtx.lineCap = 'round';
+	    newCtx.lineWidth = hex.get('scale') * 5;
+	    newCtx.beginPath();
+	    newCtx.moveTo( start.get('x'), start.get('y') );
+	    newCtx.bezierCurveTo( points.a.get('x'), points.a.get('y'), points.b.get('x'), points.b.get('y'), end.get('x'), end.get('y') );
+	    newCtx.stroke();
 
+	    ctx.drawImage( canvas, 0,0 );
+
+
+	    return grid;
+	}
 });
