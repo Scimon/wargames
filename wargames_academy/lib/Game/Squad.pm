bf8139d30c3123ea37c89095cf0a21acc73d4a86
Simon Proctor <scimon@khanate.co.uk>
2012-05-18 13:06:57 +0100

    Hmm

--- wargames_academy/lib/Game/Squad.pm
+++ wargames_academy/lib/Game/Squad.pm
@@ -5,6 +5,8 @@
 	use Game::Troop;
 
 	has 'troop' => is => 'rw', isa => 'Game::Troop'; 
+	has 'weapon' => is => 'rw', isa => 'Game::Weapon';
+	has 'armour' => is => 'rw', isa => 'Game::Armour';
 	has 'squad_size' => is => 'rw', isa => 'Int', default => 1;
 
 	'module returns true';
